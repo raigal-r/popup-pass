f93156fdee0ec4610dbaee2ebe5d71fd72f93def
raigal-r <raquel.raigal@outlook.es>
2024-11-08 02:35:53 +0700

    contracts finished

--- packages/foundry/contracts/MyPassport.sol
+++ packages/foundry/contracts/MyPassport.sol
@@ -6,17 +6,15 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyPassport is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC721("MyToken", "MTK")
-        Ownable(initialOwner)
+contract MyPassport is ERC721, ERC721URIStorage, ERC721Burnable {
+    constructor()
+        ERC721("PassportAleph", "AGMI")
     {}
 
-    function safeMint(address to, uint256 tokenId, string memory uri)
+    function mint(address to, uint256 tokenId, string memory uri)
         public
-        onlyOwner
     {
-        _safeMint(to, tokenId);
+        _mint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
